136ea8d95e8c89b64a06b103b2a5391c419b377d
ITboo <annaacdc@tut.by>
2023-01-03 20:21:06 +0300

    feat: add controller

---
+++ src/controllers/userController.ts
@@ -0,0 +1,22 @@
+// Контроллер обрабатывает входящие запросы.
+
+//import users from '../models/userModel';
+import { IncomingMessage, ServerResponse } from 'http';
+
+//import {v8} from 'uuid';
+
+export async function getUsers(req: IncomingMessage, res: ServerResponse){
+
+};
+export async function getUser(req: IncomingMessage, res: ServerResponse, id: string){
+
+};
+export async function createUser(req: IncomingMessage, res: ServerResponse){
+
+};
+export async function updateUser(req: IncomingMessage, res: ServerResponse, userId: string): Promise<void> {
+
+};
+export async function deleteUser(req: IncomingMessage, res: ServerResponse, id: string){
+
+};
