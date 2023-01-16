53c5db11da9f0924b49424fa97bc5b3d8145fae4
ITboo <annaacdc@tut.by>
2023-01-16 22:04:05 +0300

    refactor: cluster

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,17 @@ export const server = createServer(async (req, res) => {
 const numCpu = os.cpus().length;
 
 if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running. Wait for workers...`);
+
   for (let i = 0; i < numCpu; i++) {
     cluster.fork();
   }
+  cluster.on('exit', (worker, code, signal) => {
+  console.log(`Worker ${worker.process.pid} died`);
+  });
+
 } else {
   server.listen(port, () => {
-    console.log(`${process.pid} running at port http://localhost:${port}/`);
+    console.log(` Worker ${process.pid} running at port http://localhost:${port}/`);
   });
-}
+};
