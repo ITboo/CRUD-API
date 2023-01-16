22eecfc4b9a07586442822f205663920649ebb1c
ITboo <annaacdc@tut.by>
2023-01-16 21:22:13 +0300

    init: start scaling with cluster

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import {createServer} from "http";
+import { createServer } from "http";
 import { route } from "./router/router";
-/*import cluster from "cluster";
-import * as os from "os";*/
+import cluster from "cluster";
+import * as os from "os";
 
 const port = process.env.PORT || 4000;
 
@@ -9,6 +9,19 @@ export const server = createServer(async (req, res) => {
   await route(req, res);
 });
 
-server.listen(port, () => {
+/*server.listen(port, () => {
     console.log(`Server running at port ${port}`);
+  });*/
+
+
+const numCpu = os.cpus().length;
+
+if (cluster.isPrimary) {
+  for (let i = 0; i < numCpu; i++) {
+    cluster.fork();
+  }
+} else {
+  server.listen(port, () => {
+    console.log(`${process.pid} running at port http://localhost:${port}/`);
   });
+}
