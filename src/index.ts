fd5cc6c8383794040f56db9a554e4d5250717028
ITboo <annaacdc@tut.by>
2023-01-17 00:21:21 +0300

    feat: add test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { createServer } from "http";
 import { route } from "./router/router";
-import cluster from "cluster";
-import * as os from "os";
 
 const port = process.env.PORT || 4000;
 
@@ -9,25 +7,8 @@ export const server = createServer(async (req, res) => {
   await route(req, res);
 });
 
-/*server.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running at port ${port}`);
-  });*/
-
-
-const numCpu = os.cpus().length;
-
-if (cluster.isPrimary) {
-  console.log(`Primary ${process.pid} is running. Wait for workers...`);
-
-  for (let i = 0; i < numCpu; i++) {
-    cluster.fork();
-  }
-  cluster.on('exit', (worker, code, signal) => {
-  console.log(`Worker ${worker.process.pid} died`);
   });
 
-} else {
-  server.listen(port, () => {
-    console.log(` Worker ${process.pid} running at port http://localhost:${port}/`);
-  });
-};
+
