825d0b3c33f06c584ff55c0b33acb65e908266fa
ITboo <annaacdc@tut.by>
2024-02-12 13:26:03 +0300

    chore: update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,18 @@
 import { createServer } from "http";
 import dotenv from "dotenv";
 import { route } from "./router/router";
-import { startMulti } from "./loader";
 
-const port =  parseInt(process.env.PORT) || 4000;
+const port = parseInt(process.env.PORT) || 4000;
 
 dotenv.config();
-
-const isMulti = process.argv.includes("--multi") ?? false;
 
 export const server = createServer(async (req, res) => {
   await route(req, res);
 });
 
-if (isMulti) {
-  startMulti(port);
-} else {
-  server.listen(port, () => {
-    console.log(`Server running at port ${port}.`);
-  });
-}
+server.listen(port, () => {
+  console.log(`Server running at port ${port}.`);
+});
 
 process.on("SIGINT", () => {
   console.log(`Process closed`);
